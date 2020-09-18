ce31c1d468a09f42b9d5a1dd972f9a829e3cb3e4
Samuel Hinkhouse <hinkhouse.samuel@gmail.com>
2020-09-18 11:03:53 -0500

    Updates

---
+++ precache-manifest.785c5c7015f562cb182bb27b931241af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba7fe312ee2063ea84764696c6e2e76",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "522aa6c0552a824f003b",
+    "url": "/first-react-project/static/css/main.8868ddd6.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/first-react-project/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-project/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522aa6c0552a824f003b",
+    "url": "/first-react-project/static/js/main.5a24749c.chunk.js"
+  },
+  {
+    "revision": "978fd681770c8aabf459",
+    "url": "/first-react-project/static/js/runtime-main.d3092240.js"
+  }
+]);
